c8af35fcf65f4fbf560751c34ca292d19c77354d
Ethan Johnston <ethan_johnston13@hotmail.com>
2021-11-19 16:21:44 -0800

    intializing sound effect

--- scripts/main.js
+++ scripts/main.js
@@ -285,3 +285,8 @@ editModalSaveBtn.addEventListener("click", saveEditedInfoToFirestore);
 //         });
 //     }
 //   });
+
+function play() {
+  var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+  audio.play();
+}
