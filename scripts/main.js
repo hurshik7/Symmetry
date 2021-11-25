d7b08b00bd1bd6a8a10abf6dcfbc903c9d7f451c
Shey Barpagga <sheybarpagga@gmail.com>
2021-11-25 13:23:12 -0800

    did nothing

--- scripts/main.js
+++ scripts/main.js
@@ -318,7 +318,7 @@ firebase.auth().onAuthStateChanged(user => {
     currentUser = db.collection("users").doc(user.uid)
     currentUser.get().then(userDoc => {
       var back_img_name = userDoc.data().background;
-      var back_img_url = storage.ref(back_img_name);
+      //var back_img_url = storage.ref(back_img_name);
       user_img = storage.ref().child(back_img_name)
       user_img.getDownloadURL().then(function(result) {
         document.body.style.backgroundImage = 'url('+result+')'
